6b9f67a32ffd2428131e9527cfb2ab8291e56d80
JKhandelwal <jk218@st-andrews.ac.uk>
2017-11-05 15:40:07 +0000

    gitignore

--- src/HammingCodes.java
+++ src/HammingCodes.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 /**
  * Created by jalajkhandelwal on 05/11/2017.
  */
 public class HammingCodes {
+
+    public static void main(String[] args) {
+        boolean[] message = {true, false, true, false};
+
+        boolean[] t =  makeChunks(message,3);
+        System.out.println(Arrays.toString(t));
+    }
+
+
+    private static boolean[]makeChunks(boolean[] array, int numParityBits) {
+        boolean[] newArray = new boolean[array.length+numParityBits];
+        int count = 0;
+        for (int i = 0; i < newArray.length;i++){
+            if (i %2 != 0){
+                newArray[i] = array[count++];
+            }
+        }
+        return newArray;
+    }
 }
